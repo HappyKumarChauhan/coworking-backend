8a30e9aec999c6e442498cb1dea5e5a567446594
HappyKumarChauhan <kumarhappy42000@gmail.com>
2024-12-23 16:03:47 +0530

    Saved Changes

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app=express()
 const {connectToMongo}=require('./services/db')
 const mongoURI="mongodb+srv://happychauhan:Happy%23203@app0.w86uy.mongodb.net/App0"
 const userRouter=require('./routes/userRouter')
+app.use(cors());
 connectToMongo(mongoURI).then(()=>{
     console.log("Mongodb connected successfully.")
 })
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use('/users',userRouter)
